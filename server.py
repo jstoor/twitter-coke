d7ccd254d78526c0172fae83b3eb47e460dea931
Jay Toor <jay@rolepoint.com>
2013-06-10 11:47:19 +0100

    minor changes

---
+++ server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python 
+
+from flask import Flask, jsonify, render_template, request
+app = Flask(__name__)
+
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
+
+
+
+@app.route('/')
+def index():
+    data=[{"message": "Tokyo"}, {"message": "AC"}]
+    return render_template('index.html', navigation=data)  
+
+
+
+if __name__ == "__main__":
+    app.run(port=7000)
